1025a70bee6a7191b45ab4a7a29fe4712c19b324
manuel <manuel@danielcastelao.org>
2021-02-22 11:52:17 +0100

    Exemplo de informes, pintando texto, documentos con platypus e recollida de datos dende BD

--- segundoInforme.py
+++ segundoInforme.py
@@ -4,12 +4,21 @@ from reportlab.lib.pagesizes import A4
 
 imaxes = []
 
-imaxe = Image (0,0, 500, 102, "/home/manuel/Imaxes/logo-daniel-castelao.png" )
+imaxe = Image (0,0, 500, 102, "/home/manuel/Descargas/logoCole.jpg" )
 
 debuxo  = Drawing (500,102)
+debuxo.translate (0,700)
 debuxo.add (imaxe)
-
 imaxes.append (debuxo)
+
+debuxo2 = Drawing (250,51)
+debuxo2.add(imaxe)
+debuxo2.rotate (45)
+debuxo2.translate (500,50)
+debuxo2.scale (0.5, 1.5)
+
+imaxes.append (debuxo2)
+
 
 documento = Drawing (A4[0], A4[1])
 for elemento in imaxes:
